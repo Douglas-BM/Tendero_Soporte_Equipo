c6394278547d4014350ca1f5cbe5c2dd707f8829
Esteban6149 <71068687@continental.edu.pe>
2022-11-18 17:09:00 -0500

    Desarrollador v2

--- app/src/main/java/mx/com/encargalo/tendero/Inicio_sesion/ui/Soporte/sp_frgdesarrolladores.java
+++ app/src/main/java/mx/com/encargalo/tendero/Inicio_sesion/ui/Soporte/sp_frgdesarrolladores.java
@@ -1,14 +1,27 @@
 package mx.com.encargalo.tendero.Inicio_sesion.ui.Soporte;
 
+import android.app.ProgressDialog;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+
+import mx.com.encargalo.tendero.Inicio_sesion.Entidad.sp_Entidaddesarrollador;
+
+import java.util.ArrayList;
+
 import mx.com.encargalo.R;
+import mx.com.encargalo.tendero.Inicio_sesion.ui.Soporte.Util.Util;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -16,6 +29,15 @@ import mx.com.encargalo.R;
  * create an instance of this fragment.
  */
 public class sp_frgdesarrolladores extends Fragment {
+
+    RecyclerView rclvdesarrollador;
+    ArrayList<sp_Entidaddesarrollador> listadesarrollador;
+    ProgressDialog progress;
+    RequestQueue request;
+    JsonObjectRequest jsonObjectRequest;
+
+    Bundle bundle;
+
 
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -60,7 +82,22 @@ public class sp_frgdesarrolladores extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_sp_frgdesarrolladores, container, false);
+        View view = inflater.inflate(R.layout.fragment_sp_frgdesarrolladores, container, false);
+        listadesarrollador = new ArrayList<>();
+        rclvdesarrollador = view.findViewById(R.id.rclv_desarrolladores);
+        rclvdesarrollador.setLayoutManager(new LinearLayoutManager(this.getContext()));
+        rclvdesarrollador.setHasFixedSize(true);
+        request= Volley.newRequestQueue(getContext());
+
+        Mostrarlistapreguntafrecuentes();
+        return view;
+    }
+
+    private void Mostrarlistapreguntafrecuentes() {
+        progress= new ProgressDialog(getContext());
+        progress.setMessage("Consulta datos");
+        progress.show();
+        String url= Util.RUTA+"web service";
+        url=url.replace(" ","%20");
     }
 }
